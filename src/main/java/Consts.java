0e268a5a059b9f0adebc4e7e79c1cd040fc1bdab
Juliana8b <167132725+JulianaBurakova@users.noreply.github.com>
2024-06-30 21:08:38 -0500

    api homework

---
+++ src/main/java/Consts.java
@@ -0,0 +1,5 @@
+public class Consts {
+    public static final String API_KEY = "UzY3lbNM7fJBk20s6ThE1I3CUhUqTCGt";
+    public static final String BASE_URL = "https://api.apilayer.com/currency_data";
+    public static final String INVALID_KEY = "INVALID_KEY";
+}
